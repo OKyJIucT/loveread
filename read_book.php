59bfb0614bd9300e06179d010ab87ac4074c2eb2
Kohone <okyjiuct@gmail.com>
2017-02-06 10:05:48 +0200

    charset fix

--- read_book.php
+++ read_book.php
@@ -40,7 +40,7 @@ if ($p > $data['count']) {
     $data['content'] = '<h1>Выбранной страницы не существует</h1>';
 }
 
-echo $content;
+echo $data['content'];
 
 exit;
 
@@ -54,7 +54,7 @@ exit;
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?= $data['title']; ?></title>
-        <link rel="stylesheet" href="https://yastatic.net/bootstrap/3.3.6/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css"/>
         <!-- Custom styles for this template -->
         <link href="style.css?v=7" rel="stylesheet">
     </head>
