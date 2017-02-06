5b07d8747c73aebec31af42b750270752508ba1c
Kohone <okyjiuct@gmail.com>
2017-02-06 10:04:39 +0200

    charset fix

--- read_book.php
+++ read_book.php
@@ -40,12 +40,17 @@ if ($p > $data['count']) {
     $data['content'] = '<h1>Выбранной страницы не существует</h1>';
 }
 
+echo $content;
+
+exit;
+
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <meta charset=utf-8>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?= $data['title']; ?></title>
